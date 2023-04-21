bd85308e6dfee8dc87ba2bfaf8e9944e05244a83
enioCarloss <eniocarloss@gmail.com>
2023-04-21 17:42:33 -0300

    entrega Sprint4 Refatoração do Projeto TAF

--- script.js
+++ script.js
@@ -1,10 +1,73 @@
 // Implemente aqui as funções
 
+//teste de abdominais
+function testAbs(gender,abs) {
+    if (gender === "male") {
+        return abs >= 41
+    } else if (gender === "female") {
+        return abs >= 41
+    }
 
 
+}
+//teste de altura
+function maxHeigth(gender, height) {
+    if (gender === "male") {
+        return height >= 1.70
+    } else if (gender === "female") {
+        return height >= 1.60
+
+    }
+
+}
+//teste de barras
+function testBar(gender, barReps, barSeconds) {
+    if (gender === "male") {
+        return barReps <= 6 || barSeconds >= 15
 
 
-function areCandidateResultsValid() {
+    } else if (gender === "female") {
+        return barReps <= 5 || barSeconds >= 12
+    }
+
+}
+//teste de corrida e distancia
+function testRun(gender, runDistance, runTime) {
+    if (gender === "male") {
+
+        return runDistance >= 3000 && runTime <= 720 || runDistance >= 5000 && runTime <= 1200;
+
+    } else if (gender === "female") {
+
+        return runDistance >= 4000 && runTime <= 900 || runDistance >= 6000 && runTime <= 1320;
+    }
+
+}
+
+function testSwim(gender, swimDistance, swimTime, diveTime) {
+    if (gender === "male") {
+        return swimDistance >= 100 && swimTime <= 60 || diveTime <= 30;
+
+    } else if (gender === "female") {
+        return swimDistance >= 100 && swimTime <= 60 || diveTime <= 30;
+    }
+
+}
+
+
+function areCandidateResultsValid(gender, height, barReps, barSeconds, abs, runDistance, runTime, swimDistance, swimTime, diveTime,) {
+ //atribuindo variveis as funções
+    let resultHeight = maxHeigth(gender, height)
+    let resultTestBar = testBar(gender, barReps, barSeconds)
+    let resultAbs = testAbs(gender, abs)
+    let resultRun = testRun(gender, runDistance, runTime)
+    let resultSwim = testSwim(gender, swimDistance, swimTime, diveTime)
+//teste condicional de saída se verdadeiro ou falso
+    if (resultHeight && resultTestBar && resultAbs && resultRun && resultSwim) {
+        return true;
+    } else {
+        return false;
+    }
 
 }
 
@@ -21,6 +84,21 @@ const areCandidateValid = areCandidateResultsValid(
     process.argv[9], // Distancia do nado em metros
     process.argv[10], // Tempo total de nado em segundos
     process.argv[11], // Tempo total de mergulho em segundos
+
+
 );
 
 // Chame aqui a função que mostra o resultado no console
+
+//função para chamada no console.log()convertendo em letras e caixa Alta
+
+function showMessage(message) {
+    console.log(message.toString().toUpperCase());
+}
+showMessage(areCandidateValid)
+
+
+
+
+
+
